d165097710e1d56a32e3e6252fa681cd03668331
Sana0104 <sanatabasum693@gmail.com>
2023-12-05 16:20:22 +0530

    Updated 404 page

--- src/COMMON/404Page.jsx
+++ src/COMMON/404Page.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from 'react';
 import './404Page.css';
+ 
 function NotFound() {
-    return (
-        <div>
-            <h1>Looks Like Your Lost</h1><p class="zoom-area">
-                <b>Opps!</b> Page Not Found </p>
-                <section class="error-container"> 
-                 <span class="four">
-                    <span class="screen-reader-text">4</span>
-                    </span>  <span class="zero"><span class="screen-reader-text">0</span></span>
-                      <span class="four"><span class="screen-reader-text">4</span></span></section>
+  return (
+    <div className="not-found-container">
+      <div className="error-container">
+        <div className="digit">4</div>
+        <div className="digit">0</div>
+        <div className="digit">4</div>
+      </div>
+      <br/>
+      <br/>
+      <div className="text-container">
+        <h1><b>OOPS! Looks Like You're Lost</b></h1>
+        <p className="zoom-area">
+          The page you are looking for might be under construction or doesn't exist.
+        </p>
+        <br/>
+        <div className="link-container">
+          <a href="/" className="more-link">
+            Return Home
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
+ 
 export default NotFound;
