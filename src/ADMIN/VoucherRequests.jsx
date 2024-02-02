14f09aa8ddd9e3d84dde7f4d4d7329385a757ce2
Sana0104 <sanatabasum693@gmail.com>
2024-02-02 11:57:43 +0530

    changes in dashboard navbar styling

--- src/ADMIN/VoucherRequests.jsx
+++ src/ADMIN/VoucherRequests.jsx
@@ -341,9 +341,9 @@ const [denyConfirmationVisible, setDenyConfirmationVisible] = useState(false);
 </Modal>
  
  
-      <div className="navbar" style={{ backgroundColor: "rgb(112, 183, 184)", width:"auto" }}>
+      <div className="navbar" style={{ backgroundColor: "rgb(112, 183, 184)", width: "auto" }}>
  
-        <div className="user-info" style={{ marginLeft: "10px" }}>
+        <div className="user-info" style={{ marginLeft: "20px" }}>
           <p id="name">Welcome!!</p>
           <p id="date">
             {currentTime.toLocaleTimeString(undefined, timeOptions)}{" "}
@@ -607,7 +607,7 @@ const [denyConfirmationVisible, setDenyConfirmationVisible] = useState(false);
  
       </div>
  
-      <div className="footer-div" style={{ "height": "40px", "marginTop": "30px"}}>
+      <div className="footer-div" style={{ "height": "35px", "marginTop": "15px"}}>
         <footer>
           <p>&copy; 2024 Capgemini. All rights reserved.</p>
         </footer>
